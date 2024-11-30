9d6c6fd985d377f5b3944dd17848d47541c9d3db
Lealuski <adavidlealf@gmail.com>
2024-11-30 18:23:21 -0500

    Creacion de modulos
    * Modulo rooms
    * Modulo sessions
    * Modulo curriculum

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RoomsModule } from './rooms/rooms.module';
+import { CurriculumModule } from './curriculum/curriculum.module';
+import { SessionsModule } from './sessions/sessions.module';
 
 @Module({
-  imports: [],
+  imports: [RoomsModule, CurriculumModule, SessionsModule],
 })
 export class AppModule {}
