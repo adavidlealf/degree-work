248ca4413353afceb66aa92938e03fef3c21d60a
Lealuski <adavidlealf@gmail.com>
2024-12-01 20:06:47 -0500

    Create building.service.ts

---
+++ src/rooms/services/building.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BuildingEntity } from '../entities/Building.entity';
+import { Repository } from 'typeorm';
+import { CreateBuildingDto } from '../dto/create-Building-dto';
+import { CampusEntity } from '../entities/campus.entity';
+
+@Injectable()
+export class BuildingService {
+
+    constructor(
+        @InjectRepository(BuildingEntity)
+        private readonly buildingRepo: Repository<BuildingEntity>,
+        
+        @InjectRepository(CampusEntity)
+        private readonly campusRepo: Repository<CampusEntity>,
+    ){}
+
+    async getAll(): Promise<BuildingEntity[]> {
+        return await this.buildingRepo.find({
+            relations: {
+                campus: true,
+                rooms: true,
+            }
+        });
+    }
+
+    async getById(id: number): Promise<BuildingEntity> {
+        return await this.buildingRepo.findOne({
+            where: {
+                id: id
+            },
+            relations: {
+                campus: true,
+                rooms: true,
+            }
+        })
+    }
+
+    async createOne(newDto: CreateBuildingDto): Promise<BuildingEntity> {
+        const campusFound = await this.campusRepo.findOne({
+            where: {id: newDto.campus_id}
+        })
+        if(!campusFound){
+            throw new Error('Campus not found in creation of financial product');
+        }
+
+        const newEntity = new BuildingEntity();
+        newEntity.name = newDto.name;
+        newEntity.campus = campusFound;
+        return await this.buildingRepo.save(newEntity);
+    }
+
+    async updateOne(id: number, updatedDto: CreateBuildingDto): Promise<BuildingEntity> {
+        const campusFound = await this.campusRepo.findOne({
+            where: {id: updatedDto.campus_id}
+        })
+        if(!campusFound){
+            throw new Error('Campus not found in update of financial product');
+        }
+
+        const entityFound = await this.buildingRepo.findOneBy({id:id});
+        entityFound.name = updatedDto.name;
+        entityFound.campus = campusFound;
+        return await this.buildingRepo.save(entityFound);
+    }
+
+    async createMany(newDtos: CreateBuildingDto[]): Promise<BuildingEntity[]> {
+        const newEntities: BuildingEntity[] = [];
+        for(const newDto of newDtos){
+            const campusFound = await this.campusRepo.findOne({
+                where: {id: newDto.campus_id}
+            })
+            if(!campusFound){
+                throw new Error('Campus not found in bulk creation of financial product');
+            }
+    
+            const newEntity = new BuildingEntity();
+            newEntity.name = newDto.name;
+            newEntity.campus = campusFound;
+            newEntities.push(newEntity);
+        }
+        return await this.buildingRepo.save(newEntities);
+    }
+
+    async deleteAll(): Promise<void> {
+        await this.buildingRepo.clear();
+    }
+}
