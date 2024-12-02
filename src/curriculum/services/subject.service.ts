a7b0fbab5c34825e915243f8da102f84320bdea1
Lealuski <adavidlealf@gmail.com>
2024-12-01 21:22:03 -0500

    Create subject.service.ts

---
+++ src/curriculum/services/subject.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SubjectEntity } from '../entities/subject.entity';
+import { Repository } from 'typeorm';
+import { CreateSubjectDto } from '../dto/create-subject-dto';
+
+@Injectable()
+export class SubjectService {
+
+    constructor(
+        @InjectRepository(SubjectEntity)
+        private readonly subjectRepo: Repository<SubjectEntity>,
+    ){}
+
+    async getAll(): Promise<SubjectEntity[]> {
+        return await this.subjectRepo.find({
+            relations: {
+                curriculums: true,
+                sessions: true,
+            }
+        });
+    }
+
+    async getById(id: number): Promise<SubjectEntity> {
+        return await this.subjectRepo.findOne({
+            where: {
+                id: id
+            },
+            relations: {
+                curriculums: true,
+                sessions: true,
+            }
+        })
+    }
+
+    async createOne(newDto: CreateSubjectDto): Promise<SubjectEntity> {
+        const newEntity = new SubjectEntity();
+        newEntity.name = newDto.name;
+        newEntity.code = newDto.code;
+        return await this.subjectRepo.save(newEntity);
+    }
+
+    async updateOne(id: number, updatedDto: CreateSubjectDto): Promise<SubjectEntity> {
+        const entityFound = await this.subjectRepo.findOneBy({id:id});
+        entityFound.name = updatedDto.name;
+        entityFound.code = entityFound.code;
+        return await this.subjectRepo.save(entityFound);
+    }
+
+    async createMany(newDtos: CreateSubjectDto[]): Promise<SubjectEntity[]> {
+        const newEntities: SubjectEntity[] = [];
+        for(const newDto of newDtos){
+            const newEntity = new SubjectEntity();
+            newEntity.name = newDto.name;
+            newEntity.code = newDto.code;
+            newEntities.push(newEntity);
+        }
+        return await this.subjectRepo.save(newEntities);
+    }
+
+    async deleteAll(): Promise<void> {
+        await this.subjectRepo.clear();
+    }
+}
