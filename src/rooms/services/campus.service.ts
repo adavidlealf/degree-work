4afdb83422c5f37e9c4c2fd58c3c576c6e5f5a2c
Lealuski <adavidlealf@gmail.com>
2024-12-01 20:05:09 -0500

    Create campus.service.ts

---
+++ src/rooms/services/campus.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CampusEntity } from '../entities/campus.entity';
+import { Repository } from 'typeorm';
+import { CreateCampusDto } from '../dto/create-campus-dto';
+
+@Injectable()
+export class CampusService {
+
+    constructor(
+        @InjectRepository(CampusEntity)
+        private readonly campusRepo: Repository<CampusEntity>,
+    ){}
+
+    async getAll(): Promise<CampusEntity[]> {
+        return await this.campusRepo.find({
+            relations: {
+                buildings: true,
+            }
+        });
+    }
+
+    async getById(id: number): Promise<CampusEntity> {
+        return await this.campusRepo.findOne({
+            where: {
+                id: id
+            },
+            relations: {
+                buildings: true,
+            }
+        })
+    }
+
+    async createOne(newDto: CreateCampusDto): Promise<CampusEntity> {
+        const newEntity = new CampusEntity();
+        newEntity.name = newDto.name;
+        return await this.campusRepo.save(newEntity);
+    }
+
+    async updateOne(id: number, updatedDto: CreateCampusDto): Promise<CampusEntity> {
+        const entityFound = await this.campusRepo.findOneBy({id:id});
+        entityFound.name = updatedDto.name;
+        return await this.campusRepo.save(entityFound);
+    }
+
+    async createMany(newDtos: CreateCampusDto[]): Promise<CampusEntity[]> {
+        const newEntities: CampusEntity[] = [];
+        for(const newDto of newDtos){
+            const newEntity = new CampusEntity();
+            newEntity.name = newDto.name;
+            newEntities.push(newEntity);
+        }
+        return await this.campusRepo.save(newEntities);
+    }
+
+    async deleteAll(): Promise<void> {
+        await this.campusRepo.clear();
+    }
+}
